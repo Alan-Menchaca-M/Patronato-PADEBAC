01a7d9f240d8dea4d4bc85d5669651c9dcdc106b
Fco. Alan Menchaca Merino <alan_menchaca22@gero.us>
2020-05-19 22:38:57 -0500

    Se cambiaron los values, pasaron de ser las respuestas, a ser valores númericos

--- padebac-database/adopciones.sql
+++ padebac-database/adopciones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-05-2020 a las 03:24:39
+-- Tiempo de generación: 16-05-2020 a las 03:31:01
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.3
 
@@ -32,16 +32,16 @@ CREATE TABLE `formulario` (
   `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `telefono` int(50) NOT NULL,
   `nombremascota` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `ultima_vez_mascota` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `num_mascotas_convivir` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `ultima_vez_mascota` tinyint(5) NOT NULL,
+  `convivira_mas_animales` tinyint(1) NOT NULL,
   `convivir_con` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `habitara_en` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `guardian` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `compañia` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `cariñoso` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `jugueton` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `hogareño` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `energetico` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+  `habitara_en` int(3) NOT NULL,
+  `guardian` tinyint(1) NOT NULL,
+  `compañia` tinyint(1) NOT NULL,
+  `cariñoso` tinyint(1) NOT NULL,
+  `jugueton` tinyint(1) NOT NULL,
+  `hogareño` tinyint(1) NOT NULL,
+  `energetico` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 COMMIT;
 
